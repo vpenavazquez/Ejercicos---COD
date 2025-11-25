ccb8f43035538bb5e370b7f5ad73d7644d250cb2
Victor <vpenavazquez@danielcastelao.org>
2025-11-26 00:06:36 +0100

    add class barco

---
+++ Tarea Clases de Hundir la Flota/barco.py
@@ -0,0 +1,7 @@
+class Barco:
+    def __init__(self, nombre, longitud):
+        # Inicializamos los atributos obligatorios
+        self.nombre = nombre
+        self.longitud = longitud
+        self.golpes_recibidos = 0  # Empieza siempre en 0
+
