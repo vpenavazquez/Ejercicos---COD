e6d27b58d4284a3f0e66022e7cff575119b0c479
Victor <vpenavazquez@danielcastelao.org>
2025-11-26 00:24:50 +0100

    add def mostrar_tablero

--- Tarea Clases de Hundir la Flota/tablero.py
+++ Tarea Clases de Hundir la Flota/tablero.py
@@ -4,3 +4,7 @@ class Tablero:
         # Crea una lista de listas llena de 0s (Agua)
         self.casillas = [[0] * tamano for _ in range(tamano)]
 
+    def mostrar_tablero(self):
+        # Imprime cada fila de la matriz
+        for fila in self.casillas:
+            print(fila)
