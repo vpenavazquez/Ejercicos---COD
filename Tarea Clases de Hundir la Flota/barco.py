6660c9315a8eb305bad29ff7406c1b637c3f37d5
Victor <vpenavazquez@danielcastelao.org>
2025-11-26 00:21:15 +0100

    add bloque de pruebas

--- Tarea Clases de Hundir la Flota/barco.py
+++ Tarea Clases de Hundir la Flota/barco.py
@@ -15,3 +15,21 @@ class Barco:
 
     def mostrar_estado(self):
         print(f"{self.nombre} | Golpes: {self.golpes_recibidos} | ¿Hundido?: {self.esta_hundido()}")
+
+if __name__ == "__main__":
+        # Bloque de pruebas
+
+        # 1. Prueba con Submarino
+        sub = Barco("Submarino", 1)
+        sub.recibir_impacto()
+        print(f"Submarino hundido: {sub.esta_hundido()}")
+
+        # 2. Prueba con Buque
+        buque = Barco("Buque", 3)
+
+        buque.recibir_impacto()
+        print(f"Buque hundido (1 golpe): {buque.esta_hundido()}")
+
+        buque.recibir_impacto()
+        buque.recibir_impacto()
+        print(f"Buque hundido (3 golpes): {buque.esta_hundido()}")
